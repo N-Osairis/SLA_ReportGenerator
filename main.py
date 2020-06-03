54acfe588e082271ee56aa74d0d716e34bec4443
reza@nosairis.com <Parisa9468>
2020-06-03 17:24:26 +0800

    version 3(fixing pmp sla (connectivity))

--- main.py
+++ main.py
@@ -2,176 +2,199 @@ from createDocs import creatWord
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+# import time
 import datetime
 import csv
-import collections
-import gspread
-import collections, functools, operator
-from docx import Document
-from docx.shared import Inches
+# import collections
+# import gspread
+# import collections, functools, operator
+# from docx import Document
+# from docx.shared import Inches
 from collections import defaultdict
 from itertools import groupby
 from operator import itemgetter
 from tkinter import *
 import calendar
 import os
-import xlsxwriter as exwriter
-# ------------------user input box(need to update below info based on the clients)---------------------------------------------------------------
-# =========================================commen data for SLA===========================================
-sla_date=4
-sla_year=2020
-# client_name="FJB"
-#client_name="VINX-WAN"
-#client_name="VINX-EMONEY"
-client_name="PMP"
-#==============================================PMP SLA=====================================================
-if  "PMP" in client_name:
-    RD="Resources_folder\\File_resources-PMP"
-    MasterSheet=RD+"\\PMP MASTER REPORT.csv"
+# import xlsxwriter as exwriter
+
+
+# ------user input box(need to update below info based on the clients)------------
+# ====================================common data for SLA====================
+sla_date = 5
+sla_year = 2020
+# client_name = "FJB"
+# client_name = "VINX-WAN"
+# client_name = "VINX-EMONEY"
+client_name = "PMP"
+
+# =======================================PMP SLA==========================
+if "PMP" in client_name:
+    RD = "Resources_folder\\File_resources-PMP"
+    MasterSheet = RD + "\\PMP MASTER REPORT.csv"
     IncidentSheet = RD + "\\PMP Incident Report.csv"
-    SLA = 99.97  # fjb=99.5 vinx=99.7 pmp=99.97
-    # 0---->N0  1---->Site Name  6---->TNS Router ID   8---->Wired ISP   15---->SIM ISP  20---->SIM ISP
+    SLA = 99.97
+    # 0---->N0  1---->Site Name  6---->TNS Router ID   8---->Wired ISP
+    # 15---->SIM ISP  20---->SIM ISP
     MasterCo = [0, 1, 6, 8, 15, 20]
-    # 0---->N0  1---->ticket   5---->site    9---->service impact start  10---->service impact end  11---->service downtime  14---->ROOT CAUSE
+    # 0---->N0  1---->ticket   5---->site    9---->service impact start
+    # 10---->service impact end  11---->service downtime  14---->ROOT CAUSE
     IncidentCol = [0, 1, 5, 9, 10, 11, 14]
-# ===========================================================================================================
-#==============================================FJB SLA=====================================================
-if  "FJB" in client_name:
-    RD="Resources_folder\\File_resources-FJB"
-    MasterSheet=RD+"\\FJ BENJAMIN MASTERSHEET.csv"
+# ================================FJB SLA================================
+if "FJB" in client_name:
+    RD = "Resources_folder\\File_resources-FJB"
+    MasterSheet = RD + "\\FJ BENJAMIN MASTERSHEET.csv"
     IncidentSheet = RD + "\\FJB Incident Report.csv"
-    SLA = 99.97  # fjb=99.5 vinx=99.7 pmp=99.97
-    # 0---->N0  1---->site code   2---->site name   3---->store type   4---->region  12---->primary line   12----->secondary line
+    SLA = 99.97
+    # 0---->N0  1---->site code   2---->site name   3---->store type
+    # 4---->region  12---->primary line   12----->secondary line
     MasterCo = [0, 1, 2, 3, 4, 12, 18]
-    # 0---->N0  1---->ticket   4---->site    8---->service impact start  9---->service impact end  10---->service downtime  12---->ROOT CAUSE
+    # 0---->N0  1---->ticket   4---->site    8---->service impact start
+    # 9---->service impact end  10---->service downtime  12---->ROOT CAUSE
     IncidentCol = [0, 1, 4, 8, 9, 10, 12]
+# =====================================VINX-Emoney SLA=========================
+if "VINX-EMONEY" in client_name:
+    RD = "Resources_folder\\File_resources-VINXEMONEY"
+    MasterSheet = RD + "\\AEON (VINX) MASTERSHEET.csv"
+    IncidentSheet = RD + "\\Vinx Incident Report.csv"
+    SLA = 99.7
+    # 0---->N0  1---->site code   2---->site name   3---->store type
+    # 5---->region  13---->primary line   17----->secondary line   24---->secondary 3G
+    MasterCo = [0, 1, 2, 3, 5, 13, 17, 24]
+    # 0---->N0  1---->ticket   5---->site    9---->service impact start
+    # 10---->service impact end  11---->service downtime  13---->ROOT CAUSE
+    IncidentCol = [0, 1, 5, 9, 10, 11, 13]
+# =====================================VINX-WAN===============================
+if "VINX-WAN" in client_name:
+    RD = "Resources_folder\\File_resources-VINXWAN"
+    MasterSheet = RD + "\\AEON (VINX) MASTERSHEET.csv"
+    IncidentSheet = RD + "\\Vinx Incident Report.csv"
+    SLA = 99.7
+    # 0---->N0  1---->site code   2---->site name   3---->store type
+    # 6---->region  10---->primary line   16----->secondary line
+    MasterCo = [0, 1, 2, 3, 6, 10, 16]
+    # 0---->N0  1---->ticket   5---->site    9---->service impact start
+    # 10---->service impact end  11---->service downtime  13---->ROOT CAUSE
+    IncidentCol = [0, 1, 5, 9, 10, 11, 13]
 
-# ===========================================================================================================
-#==============================================VINX-Emoney SLA=====================================================
-if  "VINX-EMONEY" in client_name:
-    RD="Resources_folder\\File_resources-VINXEMONEY"
-    MasterSheet=RD+"\\AEON (VINX) MASTERSHEET.csv"
-    IncidentSheet=RD+"\\Vinx Incident Report.csv"
-    SLA=99.7 # pmp=99.97
-    # 0---->N0  1---->site code   2---->site name   3---->store type   5---->region  13---->primary line   17----->secondary line   24---->secondary 3G
-    MasterCo=[0,1,2,3,5,13,17,24]
-    # 0---->N0  1---->ticket   5---->site    9---->service impact start  10---->service impact end  11---->service downtime  13---->ROOT CAUSE
-    IncidentCol=[0,1,5,9,10,11,13]
-# ===========================================================================================================
-if  "VINX-WAN" in client_name:
-    RD="Resources_folder\\File_resources-VINXWAN"
-    MasterSheet=RD+"\\AEON (VINX) MASTERSHEET_april.csv"
-    IncidentSheet=RD+"\\Vinx Incident Report_april.csv"
-    SLA=99.7 # pmp=99.97
-    # 0---->N0  1---->site code   2---->site name   3---->store type   6---->region  10---->primary line   16----->secondary line
-    MasterCo=[0,1,2,3,6,10,16]
-    # 0---->N0  1---->ticket   5---->site    9---->service impact start  10---->service impact end  11---->service downtime  13---->ROOT CAUSE
-    IncidentCol=[0,1,5,9,10,11,13]
+
+# -------------------------------------sub functions---------------------
+# this function open the input csv file return all the data in array list
 
 
-# -------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-# ---------------------------------------sub functions-----------------------------------------------
-#this function open the input csv file return all the data in array list
-def opencsv(filetext):
-    with open(filetext) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
+def open_csv(file_text):
+    with open(file_text) as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
         datas = []
-        for i,row in enumerate(readCSV):
-            if i>0:
-             date = row
-             datas.append(date)
-             # print (date)
+        for i, row1 in enumerate(read_csv):
+            if i > 0:
+                date = row1
+                datas.append(date)
 
     return datas
 
+# this function open the input csv file return the specific column data in array list
 
-#this function open the input csv file return the specific column data in array list
-def opencsv_specifcColumn(filetext,ColList):
-    with open(filetext) as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        print(ColList)
+
+def open_csv_specifc_column(file_text, col_list):
+    with open(file_text) as csvfile:
+        read_csv = csv.reader(csvfile, delimiter=',')
+        print(col_list)
         datas = []
-        for i,row in enumerate(readCSV):
-            if i>=1:
-              date = []
-              for z in ColList:
-                date.append(row[z])
-              datas.append(date)
+        for i, row1 in enumerate(read_csv):
+            if i >= 1:
+                date = []
+                for z in col_list:
+                    date.append(row1[z])
+                datas.append(date)
     return datas
 
-def MinCalculate(str):
-   x =str.split()
-   min=(int(x[0])*1440)+(int(x[2])*60)+int(x[4])
-   return min
-def findfjbname(str,cl):
-    sn=None
-    if "::" in str:
-        sitename=str.split("::")
-        sn=sitename[len(sitename)-1]
+
+def min_calculate(str1):
+    res1 = str1.split()
+    min_value = (int(res1[0])*1440)+(int(res1[2])*60)+int(res1[4])
+    return min_value
+
+
+def find_name(str1, cl):
+    sn = None
+    if "::" in str1:
+        sitename = str1.split("::")
+        sn = sitename[len(sitename)-1]
         if "VINX-WAN" in cl or "VINX-EMONEY" in cl:
-            s=sn.split("_")
-            sn = s[len(s) - 1]
+            sp1 = sn.split("_")
+            sn = sp1[len(sp1) - 1]
         elif "PMP" in cl:
-            sn=str(sitename[1]).replace(" ","")
+            sn = str(sitename[1]).replace(" ", "")
 
     return sn
-def setcolors(row1):
-    c=[]
+
+
+def set_colors(row1):
+    c1 = []
     for it in row1:
         # print(it)
         if it >= SLA:
-            c.append('DodgerBlue')
+            c1.append('DodgerBlue')
         else:
-            # c.append('red')
-            c.append('DodgerBlue')
+            # c1.append('red')
+            c1.append('DodgerBlue')
 
-    return c
-def autolabel(ax,rects):
+    return c1
+
+
+def auto_label(ax, rects):
     """Attach a text label above each bar in *rects*, displaying its height."""
     for rect in rects:
         height = rect.get_height()
         ax.annotate('{}'.format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
+                    # 3 points vertical offset
+                    xytext=(0, 3),
                     textcoords="offset points",
                     ha='center', va='bottom')
-def make_multiline(list):
-    outputlist=[]
-    for st in list:
-     outputlist.append(st.replace('-', '\n'))
-    tlist= tuple(outputlist)
-    return  tlist
-def checkDicisExist(Dict, listD):
+
+
+def make_multiline(list1):
+    outputlist = []
+    for st in list1:
+        outputlist.append(st.replace('-', '\n'))
+    tlist = tuple(outputlist)
+    return tlist
+
+
+def check_dict_exist(dict1, listd):
     output1 = False
-    ind=None
-    if len(listD) > 0:
-        for Indx ,i in enumerate (listD):
-            if Dict['SITE'] in i[0]:
+    ind1 = None
+    if len(listd) > 0:
+        for ix, i in enumerate(listd):
+            if dict1['SITE'] in i[0]:
                 output1 = True
-                ind=Indx
-    return [output1,ind]
-def combineresult(lD1,lD2):
-    output=[]
-    for i in lD1 :
-        for j in lD2 :
-            if i['SITE'] == j['SITE'] :
-                CI=checkDicisExist(i ,output)
-                if CI[0]==False :
-                 output.append([i['SITE'],i['value'],j['value']])
-                elif CI[0]==True :
-                    print(CI[1])
-                    output[CI[1]]=[i['SITE'], i['value'], j['value']]
+                ind1 = ix
+    return [output1, ind1]
+
+
+def combine_result(ld1, ld2):
+    output = []
+    for i in ld1:
+        for j in ld2:
+            if i['SITE'] == j['SITE']:
+                dic_is_exist = check_dict_exist(i, output)
+                if dic_is_exist[0] is False:
+                    output.append([i['SITE'], i['value'], j['value']])
+                elif dic_is_exist[0] is True:
+                    print(dic_is_exist[1])
+                    output[dic_is_exist[1]] = [i['SITE'], i['value'], j['value']]
             else:
-                CI = checkDicisExist(i, output)
-                if CI[0]==False :
+                dic_is_exist = check_dict_exist(i, output)
+                if dic_is_exist[0]==False:
                     output.append([i['SITE'], i['value'], 0])
-                CI = checkDicisExist(j, output)
-                if CI[0] == False:
+                dic_is_exist = check_dict_exist(j, output)
+                if dic_is_exist[0]==False:
                     output.append([j['SITE'], 0, j['value']])
-    return  output
+    return output
+
+
 def creatingFinalList(mdf,R1):
     FL = []
     for ind in mdf.index:
@@ -191,6 +214,13 @@ def creatingFinalList(mdf,R1):
                 connectivity = mdf['primary'][ind]
             else:
                 connectivity = mdf['primary'][ind] + "/" + mdf['secondary'][ind]
+
+        elif len(mdf.columns) == 6:
+            if "N/A" in mdf['secondary 3G'][ind] :
+                connectivity = mdf['primary'][ind] + "/" + mdf['secondary'][ind]
+            else:
+                connectivity = mdf['primary'][ind] + "/" + mdf['secondary'][ind]+ "/" + mdf['secondary 3G'][ind]
+
 
         if any(d1[0] in  mdf['Site Name'][ind] for i1, d1 in enumerate(R1)) :
 
@@ -233,21 +263,29 @@ def creatingFinalList(mdf,R1):
                             str(CUSdowntime), str(Totaldown), str(uptimePercentage)]
             FL.append(Tempdict)
     return FL
-# -----------------------------------------Main code-------------------------------------------------
-# ---------------------------------------------------------------------------------------------------
 
-Incident_Row_data=opencsv_specifcColumn(IncidentSheet,IncidentCol)
-Master_Row_data=opencsv_specifcColumn(MasterSheet,MasterCo)
+
+# -----------------------------------------Main code------------------------------------
+# --------------------------------------------------------------------------------------
+
+Incident_Row_data = open_csv_specifc_column(IncidentSheet,IncidentCol)
+Master_Row_data = open_csv_specifc_column(MasterSheet,MasterCo)
 # client name may be "PMP", "FJB", "VINX"
 
-if  "VINX-EMONEY" in client_name:
-  mdf=pd.DataFrame(Master_Row_data,columns=['NO','Site Code','Site Name','Store Type','Region','primary','secondary','secondary 3G'])
-elif  "FJB" in client_name or "VINX-WAN" in client_name:
-  mdf=pd.DataFrame(Master_Row_data,columns=['NO','Site Code','Site Name','Store Type','Region','primary','secondary'])
-elif "PMP" in client_name :
-    mdf = pd.DataFrame(Master_Row_data,columns=['NO', 'Site Name', 'Router ID', 'primary','secondary','secondary 3G'])
+if "VINX-EMONEY" in client_name:
+  mdf = pd.DataFrame(Master_Row_data,
+                     columns=['NO', 'Site Code', 'Site Name', 'Store Type', 'Region',
+                              'primary', 'secondary', 'secondary 3G'])
+elif "FJB" in client_name or "VINX-WAN" in client_name:
+  mdf = pd.DataFrame(Master_Row_data,
+                     columns=['NO', 'Site Code', 'Site Name', 'Store Type', 'Region', 'primary', 'secondary'])
+elif "PMP" in client_name:
+    mdf = pd.DataFrame(Master_Row_data,
+                       columns=['NO', 'Site Name', 'Router ID', 'primary', 'secondary', 'secondary 3G'])
 # df=pd.DataFrame(Row_data,columns=['NO','TICKET','WEEK','SEVERITY','SITE','SOURCE','INCIDENT START','INCIDENT END','SERVICE-IMPACT START (10AM-10PM)','SERVICE-IMPACT END (10AM-10PM)','SERVICE DOWNTIME','ONSITE','ROOT CAUSE','REMARKS','REVISED BY'])
-idf=pd.DataFrame(Incident_Row_data,columns=['NO','TICKET','SITE','SERVICE-IMPACT START','SERVICE-IMPACT END','SERVICE DOWNTIME','ROOT CAUSE'])
+idf = pd.DataFrame(Incident_Row_data,
+                   columns=['NO', 'TICKET', 'SITE', 'SERVICE-IMPACT START',
+                            'SERVICE-IMPACT END', 'SERVICE DOWNTIME', 'ROOT CAUSE'])
 
 
 # fdf = pd.DataFrame(columns=['NO','TICKET','SITE','SERVICE-IMPACT START','SERVICE-IMPACT END','SERVICE DOWNTIME','ROOT CAUSE','Total DownTime_min']) #result after calculation of total min
@@ -256,13 +294,13 @@ incidentlistCUS=[] #customer incident
 for x in idf.index:
     if  idf['SERVICE-IMPACT START'][x]!="" and idf['SERVICE-IMPACT END'][x]!="" :
         if  "WAN" in idf['ROOT CAUSE'][x] or "wan" in idf['ROOT CAUSE'][x]:
-            totalmin= MinCalculate(idf['SERVICE DOWNTIME'][x])
+            totalmin= min_calculate(idf['SERVICE DOWNTIME'][x])
             print("{} >>>>{}-----------{}-----{}".format(x, idf['SITE'][x], idf['SERVICE DOWNTIME'][x], totalmin))
-            incidentlist.append({'NO':idf['NO'][x],'TICKET':idf['TICKET'][x],'SITE':findfjbname(idf['SITE'][x],client_name),'SERVICE-IMPACT START':idf['SERVICE-IMPACT START'][x],'SERVICE-IMPACT END':idf['SERVICE-IMPACT END'][x], 'SERVICE DOWNTIME':idf['SERVICE DOWNTIME'][x],'ROOT CAUSE': idf['ROOT CAUSE'][x],'Total DownTime_min':totalmin})
+            incidentlist.append({'NO':idf['NO'][x],'TICKET':idf['TICKET'][x],'SITE':find_name(idf['SITE'][x],client_name),'SERVICE-IMPACT START':idf['SERVICE-IMPACT START'][x],'SERVICE-IMPACT END':idf['SERVICE-IMPACT END'][x], 'SERVICE DOWNTIME':idf['SERVICE DOWNTIME'][x],'ROOT CAUSE': idf['ROOT CAUSE'][x],'Total DownTime_min':totalmin})
         else :
-            totalmin= MinCalculate(idf['SERVICE DOWNTIME'][x])
+            totalmin= min_calculate(idf['SERVICE DOWNTIME'][x])
             print("{} >>>>{}-----------{}-----{}".format(x, idf['SITE'][x], idf['SERVICE DOWNTIME'][x], totalmin))
-            incidentlistCUS.append({'NO':idf['NO'][x],'TICKET':idf['TICKET'][x],'SITE':findfjbname(idf['SITE'][x],client_name),'SERVICE-IMPACT START':idf['SERVICE-IMPACT START'][x],'SERVICE-IMPACT END':idf['SERVICE-IMPACT END'][x], 'SERVICE DOWNTIME':idf['SERVICE DOWNTIME'][x],'ROOT CAUSE': idf['ROOT CAUSE'][x],'Total DownTime_min':totalmin})
+            incidentlistCUS.append({'NO':idf['NO'][x],'TICKET':idf['TICKET'][x],'SITE':find_name(idf['SITE'][x],client_name),'SERVICE-IMPACT START':idf['SERVICE-IMPACT START'][x],'SERVICE-IMPACT END':idf['SERVICE-IMPACT END'][x], 'SERVICE DOWNTIME':idf['SERVICE DOWNTIME'][x],'ROOT CAUSE': idf['ROOT CAUSE'][x],'Total DownTime_min':totalmin})
 
 
 print("-------------------------------")
@@ -280,7 +318,7 @@ resultCUS = defaultdict(int)
 get_nameCUS = itemgetter('SITE')
 resultCUS = [{'SITE': name, 'value': str(sum(int(d['Total DownTime_min']) for d in dicts))}
            for name, dicts in groupby(sorted(incidentlistCUS, key=get_nameCUS), key=get_nameCUS)]
-finalresult=combineresult(result,resultCUS)
+finalresult=combine_result(result,resultCUS)
 
 
 print("========================================================")
@@ -331,7 +369,7 @@ if len(final_list)>0:
                      final_list[5][1], final_list[6][1], final_list[7][1], final_list[8][1], final_list[9][1])
 
 
-       colorlist= setcolors( row )
+       colorlist = set_colors(row)
        N = 10
        ii = 0
        max1=max(row)
@@ -361,7 +399,7 @@ if len(final_list)>0:
 
             row = row + (float(final_list[ii][10]),)
             lables1 = lables1 + (final_list[ii][1],)
-            colorlist = setcolors(row)
+            colorlist = set_colors(row)
             # final_list.pop(ii)
             del final_list[ii]
             ii -= 1
@@ -392,7 +430,7 @@ if len(final_list)>0:
      s = fig.add_subplot(111)
 
      rects1=s.bar(ind,np.array(row), width,color=colorlist)
-     autolabel(s,rects1)
+     auto_label(s,rects1)
      s.set_ylabel('Uptime %',fontsize=12)
      s.set_xticks(x)
      s.set_xticklabels( lables2,fontsize=12,rotation=90,ha='right',rotation_mode="anchor")
